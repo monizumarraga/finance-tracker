f914919136f9fa32e39ab174e08b6ac2984cbb2c
monizumarraga <monizumarraga.hotmail.com>
2018-07-20 11:31:15 +0000

    included user_stock association and heroku push

--- app/controllers/stock_controller.rb
+++ app/controllers/stock_controller.rb
@@ -1,10 +1,10 @@
 class StockController < ApplicationController
   def search
     if params[:stock].blank?
-      flash.now[:danger] = "You have entered an incorrect symbol"
+      flash.now[:danger] = "You have entered an empty search string"
     else
       @stock = Stock.new_from_lookup(params[:stock])
-      flash.now[:danger] = "You have entered an empty search string" unless @stock
+      flash.now[:danger] = "You have entered an incorrect symbol" unless @stock
     end
     respond_to do |format|
       format.js { render partial: 'users/result' }
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,5 +1,7 @@
 class Stock < ApplicationRecord
-    
+    has_many :user_stocks
+    has_many :users, through: :user_stocks
+
     def self.new_from_lookup(ticker_symbol)
         begin
             looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
@@ -8,4 +10,9 @@ class Stock < ApplicationRecord
             return nil
         end
     end
+    
+    def self.find_by_ticker(ticker_symbol)
+        where(ticker: ticker_symbol).first
+    end
+    
 end
